1393d99a1e698927ba71836fffdef77fe676f908
Umar Abdullayev <umarabdullayev295@gmail.com>
2026-05-21 18:41:50 +0500

    Update main.py for easier Render start command

--- main.py
+++ main.py
@@ -75,3 +75,8 @@ def export_keys():
         data = json.load(f)
     os.remove(export_path)
     return {"exported_keys": data}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
